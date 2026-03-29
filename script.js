fb0ac374d6e82eec4f7a8abafbab1f27323f1687
HarshitaKoshta <harshitakoshta2111@gmail.com>
2026-03-29 21:24:08 +0530

    page5

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// const { Scale } = require("lucide-react")
+
 function navAnimation() {
     var nav = document.querySelector("nav")
 
@@ -38,3 +40,42 @@ function navAnimation() {
     })
 }
 navAnimation()
+
+function page2Animation(){
+    var rightElems = document.querySelectorAll(".right-elem")
+
+rightElems.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        gsap.to(elem.childNodes[3],{
+            opacity:1,
+            scale:1
+        })
+    })
+    elem.addEventListener("mouseleave",function(){
+        gsap.to(elem.childNodes[3],{
+            opacity:0,
+            scale:0
+        })
+    })
+    elem.addEventListener("mousemove",function(dets){
+      gsap.to(elem.childNodes[3],{
+        x:dets.x - elem.getBoundingClientRect().x-50,
+        y:dets.y - elem.getBoundingClientRect().y-95
+      })    
+    })
+})
+}
+page2Animation()
+
+var sections = document.querySelectorAll(".sec-right")
+
+sections.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        elem.childNodes[3].style.opacity = 1
+    elem.childNodes[3].play()
+    })
+    elem.addEventListener("mouseleave",function(){
+        elem.childNodes[3].style.opacity = 0
+        elem.childNodes[3].load()
+    })
+})
